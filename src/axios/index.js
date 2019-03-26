1efb3b6aefd63e7624d6b60e1e86b4ed56415ef5
amir <you@example.com>
2019-03-26 23:57:56 +0430

    first Commit

---
+++ src/axios/index.js
@@ -0,0 +1,15 @@
+
+
+
+import  axios from 'axios' ; 
+
+
+
+const axiosInstance = axios.create({
+
+    baseURL : "localhost:8080/api"
+}) ; 
+
+
+
+export default axiosInstance ; 
---
+++ src/routes/Routes.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Aux from '../hoc/Aux';
+
+const routes = () => {
+    return ( 
+            <Aux>
+                hello      
+            </Aux>
+    );
+}
+ 
+export default routes;
