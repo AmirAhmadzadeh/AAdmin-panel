443ad83fb6e24eeeca6439e42896fc25307f2e12
amir <you@example.com>
2019-10-24 20:24:16 +0330

    making Course Component Work for deleting and creating post

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,62 +1,96 @@
 import React, { useEffect } from 'react'
+
 import { Tabs, Tab } from '@material-ui/core';
+
 import CreateCourse from './CreateCourse/CreateCourse';
+
 import CourseTable from './CourseTable/CourseTable';
+
 import { connect } from 'react-redux';
-import { fetchCourse, fetchCategory } from '../../store/action/';
+
+import { fetchCourse, fetchCategory, createCourse , deleteCourse } from '../../store/action/';
+
 import useValue from './../../hooks/useValue';
 
 
 function Course(props) {
+
     const [value, setValue] = useValue();
 
+    function handleCreateCourse(data) {
+
+        console.log(`handleCreateCourse Function  Course component `);
+
+        props.createCourse(data);
+
+        setValue(0);
+    }
+
+    function remove (id) { 
+       props.deleteCourse(id) ;       
+    }
+
     useEffect(() => {
+
         props.loadCourses();
+
         props.loadCats();
-    },[value]) ; 
+
+    }, [value]);
 
 
     function getContent() {
 
 
         if (value === 0) {
-            return <CourseTable courses={props.courses} />
+
+            return <CourseTable deleteCourse={remove}  courses={props.courses} />
+
         }
 
-        return <CreateCourse cats={props.cats} />
+        return <CreateCourse makeNewCourse={handleCreateCourse} cats={props.cats} created={props.created} />
 
     }
 
 
     return (
+
         <div className="menu">
-           
+
             <h1 className="heading__scondary heading">
                 دوره ها
+
         </h1>
 
             <Tabs
+
                 value={value}
-                
-                onChange={(e,value) => setValue(value)}
-                
+
+
+                onChange={(e, value) => setValue(value)}
+
+
                 indicatorColor="primary"
-                
+
+
+
                 textColor="primary"
-                
+
+
                 centered
+
             >
-                
+
                 <Tab label="مشاهده ی لیست دوره ها" className="menu__tab-label" />
-                
+
                 <Tab label="ایجاد دوره جدید " className="menu__tab-label" />
-            
+
             </Tabs>
-           
+
             <div className="menu__cmpArea">
-              
+
                 {getContent()}
-           
+
             </div>
 
         </div>
@@ -67,17 +101,31 @@ function Course(props) {
 
 
 const mapDispatchToProps = dispatch => {
+
     return {
+
         loadCourses: () => dispatch((fetchCourse())),
-        loadCats: () => dispatch(fetchCategory())
+
+        loadCats: () => dispatch(fetchCategory()),
+
+        createCourse: (data) => dispatch(createCourse(data)) ,  
+
+        deleteCourse : (_id) => dispatch(deleteCourse(_id))
     }
 }
 
 const mapStateToProps = state => {
 
     return {
+
         courses: state.course.courses,
+
+        error: state.course.error,
+
+        created: state.course.created,
+
         cats: state.cats.categories
+
     }
 }
 
--- src/containers/Course/CreateCourse/CreateCourse.js
+++ src/containers/Course/CreateCourse/CreateCourse.js
@@ -1,215 +1,299 @@
 
 
-
 import React, { useEffect } from 'react'
+
 import {
   Button,
   FormGroup, TextField, FormControl,
-  Select, MenuItem, Input,
-  Checkbox, ListItemText
+  Select,
+  Input,
+  MenuItem,
+  Checkbox,
+  ListItemText,
+  InputLabel
 } from '@material-ui/core';
 
-import ImageUploader from './../../../components/ImageUploader/ImageUploader';
+import useInputState from './../../../hooks/useInputState';
+
+import useValueState from './../../../hooks/useValue';
+
+import useArrayValueState from './../../../hooks/useArrayValueState';
+
+import useFilePicker from './../../../hooks/useFilePicker';
+
 
 import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator';
-
-import useInputState  from './../../../hooks/useInputState' ;
-
-function CreateCourse (props) {
+import useBoolean from '../../../hooks/useBoolean';
 
 
-  // const [title , setTitle] = useInputState(null) ; 
 
+export default function ({ makeNewCourse, cats }) {
+
+  const [title, setTitle] = useInputState('');
+
+  const [price, setPrice] = useValueState(null);
+
+  const [slug, setSlug] = useInputState('');
+
+  const [tag, setTag] = useInputState('');
+
+  const [kindOfCourse, setKindOfCourse] = useValueState('free');
+
+  const [categories, setCategories] = useArrayValueState([]);
+
+  const [courseImage, setImage] = useFilePicker(null);
+
+  const [canSendReq, toggleCanUse, setCanSendReq] = useBoolean(false);
+
+  const [errorMsg, setErrorMsg] = useValueState(null);
+
+  const [courseText, setCourseText] = useInputState('');
+
+
+  function handleSubmit() {
+
+    if (canSendReq) {
+      setErrorMsg(null);
+      console.log('helooooooo amir')
+
+      const data = {
+        slug, categories, title, type: kindOfCourse, body: courseText, price, tags: tag, file: courseImage
+      }
+      // console.log('amir is hre ', data);
+      makeNewCourse(data);
+
+    } else {
+      setErrorMsg('Error In Creating Course Please Check youre Inputs');
+    }
+  }
+
+
+
+
+  const isNumber = (n) => (!isNaN(parseFloat(n)) && isFinite(n))
+
+
+  useEffect(() => {
+    ValidatorForm.addValidationRule('isNum', (value) => {
+      if (isNumber(value)) return true;
+      return false
+    });
+
+    if (categories.length === 0 || courseImage === null) setCanSendReq(false);
+    else if (categories.length !== 0 && courseImage) setCanSendReq(true);
+
+
+  });
 
   return (
-
     <React.Fragment>
 
-      <FormControl className="form__controller">
-     
-        <TextValidator
-          label="عنوان دوره"
-          className="form__controller--inp"
-          // onChange={(e) => {setTitle(e.target.value)}}
-          name="title"
-          // value={title}
-          validators={['required']}
-          errorMessages={['this field is required']}
-        />
-      </FormControl>
+      <h1> {errorMsg ? errorMsg : null} </h1>
+      <ValidatorForm onSubmit={handleSubmit}>
 
-     
+        <FormGroup>
+          <FormControl className="form__controller">
+            <TextValidator
+              label="عنوان دوره"
+              className="form__controller--inp"
+              onChange={e => { setTitle(e.target.value) }}
+              name="title"
+              validators={['required']}
+              errorMessages={['لطفا چیزی  را بنویسید !']}
+              value={title}
+              fullWidth
+              margin="normal"
+              variant="filled"
+              // placeholder="Placeholder"
+              helperText="Full width!"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </FormControl>
+        </FormGroup>
 
-      <Button> sakhtan</Button>
+        <FormControl className="form__controller">
 
+          <TextValidator
+            value={price}
+            label="قیمت دوره"
+            className="form__controller--inp"
+            onChange={(e) => { setPrice(e.target.value) }}
+            name="price"
+            validators={['required', 'isNum']}
+            errorMessages={['لطفا چیزی  را بنویسید !', 'لطفا رقم مبغ دوره را وارد کنید ']}
+            // fullWidth
+            margin="normal"
+            variant="filled"
+            // placeholder="Placeholder"
+            helperText="Full width!"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </FormControl>
+
+        <FormControl className="form__controller">
+
+          <TextValidator
+            value={slug}
+            label="اسلاگ دوره"
+            className="form__controller--inp"
+            onChange={(e) => { setSlug(e.target.value) }}
+            validators={['required']}
+            errorMessages={['لطفا چیزی  را بنویسید !']}
+            name='slug'
+            // fullWidth
+            margin="normal"
+            variant="filled"
+            // placeholder="لطفا اسلاگ دوره را وارد کنید!"
+            helperText="Full width!"
+            InputLabelProps={{
+              shrink: true,
+            }}
+
+          />
+        </FormControl>
+
+        <FormControl className="form__controller">
+
+          <TextValidator
+            label="تگ های دوره"
+            className="form__controller--inp"
+            name='tag'
+            value={tag}
+            onChange={e => { setTag(e.target.value) }}
+            validators={['required']}
+            errorMessages={['لطفا چیزی  را بنویسید !']}
+            margin="normal"
+            variant="filled"
+            // placeholder="لطفا اسلاگ دوره را وارد کنید!"
+            helperText="Full width!"
+            InputLabelProps={{
+              shrink: true,
+            }}
+
+          />
+        </FormControl>
+        <FormGroup>
+          <FormControl className="form__controller" variant="filled">
+            <label className="form__controller--label">
+              نوع
+              دوره
+            </label>
+            <Select
+              className="form__controller--select"
+              native
+              value={kindOfCourse}
+              onChange={(e) => { setKindOfCourse(e.target.value); }}
+
+              inputProps={{
+                name: 'selectedKindOfCourse'
+                // id: 'age-native-simple',
+              }}
+
+            >
+              <option value="free" className="form__controller--option">  رایگان </option>
+              <option value="vip" className="form__controller--option">  دسترسی با عضویت ویژه </option>
+              <option value="cash" className="form__controller--option">نقدی </option>
+
+            </Select>
+          </FormControl>
+        </FormGroup>
+        <FormGroup>
+
+          <FormControl variant="filled" className="form__controller" style={{}}>
+            <label className="form__controller--label">
+              دسته ها
+            </label>
+            <Select
+              multiple
+              value={categories}
+              onChange={(e) => setCategories(e.target.value)}
+              input={<Input id="select-multiple-checkbox" />}
+              renderValue={selected => selected.join(', ')}
+            // MenuProps={MenuProps}
+            >
+              {
+                cats ? cats.map(catItem => (
+                  <MenuItem key={catItem.name} value={catItem._id}>
+                    <Checkbox checked={categories.indexOf(catItem._id) > -1} />
+                    <ListItemText primary={catItem.name} />
+                  </MenuItem>
+                )) : null
+                }
+                
+            </Select>
+          </FormControl>
+        </FormGroup>
+
+        <FormGroup>
+          {/* <ImageUploader
+            // errorOcurred={handleImagePickerError}
+            imageSelected={handlePickImage}    
+            /> */}
+          <label className="form__controller--label">
+            تصویر دوره
+            </label>
+          {/* <TextValidator
+            // value={JSON.stringify(courseImage)}
+            type="file"
+            name="file"
+            onChange={(e) => { setImage(e.target.files[0]) }}
+            validators={['required']}
+            errorMessages={['لطفا چیزی  را بنویسید !']}
+            variant="filled"
+          /> */}
+
+          <Input
+            // value={courseImage}
+            type="file"
+            name="file"
+            onChange={(e) => {
+              // console.log(e.target.files[0]) ; 
+              setImage(e.target.files[0])
+            }}
+            variant="filled"
+
+          />
+        </FormGroup>
+
+        <FormGroup style={{ margin: "4rem 0 0 0" }}>
+          <label className="form__controller--label"> متن دوره </label>
+
+
+          <TextValidator
+            multiline
+            name="courseText"
+            onChange={(e) => { setCourseText(e.target.value) }}
+            validators={['required']}
+            errorMessages={['لطفا چیزی  را بنویسید !']}
+            value={courseText}
+            validators={['required']}
+            errorMessages={['لطفا چیزی  را بنویسید !']}
+          />
+
+        </FormGroup>
+        <Button
+          disabled={!canSendReq}
+          type="submit"
+          color="primary"
+          variant="contained"
+          style={{
+            fontSize: "1.5rem"
+            , margin: "3.5rem 0"
+          }}> ایجاد دوره</Button>
+      </ValidatorForm>
     </React.Fragment>
-
   )
 }
 
 
 
 
-export default CreateCourse;
 
 
 
 
 
-
-
-
-
-
-
-
-
-  // const [kindOfCourse , setKindOfCourse] = useValueState('free') ;
-  // const [categories , setCategories ] = useArrayValueState([]) ; 
-  // const [price , setPrice] = useValueState(0) ; 
-  // const [courseText, setCourseText ] = useInputState('') ; 
-  // const [courseImage , setImage] = useFilePicker() ;  
-
-
-
-  // handleChange = (event) => {
-  //   this.setState({
-  //     [event.target.name]: event.target.value
-  //   })
-  // }
-
-
-  // handleChangeMultiple = (event) => {
-  //   this.setState({
-  //     categories: event.target.value
-  //   })
-  // }
-
-
-  // useEffect(() => {
-  //     console.log(props.cats , 'amir is here'); 
-  // }) ;  
-/*
-
- {/* <FormControl className="form__controller">
-
-        <TextField
-          label="قیمت دوره"
-          className="form__controller--inp"
-          onChange={this.handleChange}
-          inputProps={{
-            name: "price"
-          }}
-        />
-      </FormControl> 
-
-       {/* <FormControl className="form__controller">
-
-        <TextField
-          label="اسلاگ دوره"
-          className="form__controller--inp"
-          onChange={this.handleChange}
-          inputProps={{
-            name: "slug"
-          }}
-        />
-      </FormControl>  
-      
-       <FormControl className="form__controller">
-
-        <TextField
-          label="تگ های دوره"
-          className="form__controller--inp"
-          onChange={this.handleChange}
-          inputProps={{
-            name: "tags"
-          }}
-        />
-      </FormControl> 
-     
-      <FormGroup>
-      {/*  <FormControl className="form__controller">
-          <label className="form__controller--label">
-            نوع
-            دوره
-            </label>
-          <Select
-            className="form__controller--select"
-            native
-            value={this.state.selectedKindOfCourse}
-            onChange={this.handleChange}
-
-            inputProps={{
-              name: 'selectedKindOfCourse'
-              // id: 'age-native-simple',
-            }}
-          >
-            <option value="free" className="form__controller--option">  رایگان </option>
-            <option value="vip" className="form__controller--option">  دسترسی با عضویت ویژه </option>
-            <option value="cash" className="form__controller--option">نقدی </option>
-
-          </Select>
-        </FormControl> 
-
-
-        {/* <FormControl className="form__controller">
-          <label className="form__controller--label">
-            دسته ها
-            </label>
-          <Select
-            multiple
-            value={this.state.categories}
-            onChange={this.handleChangeMultiple}
-            input={<Input id="select-multiple-checkbox" />}
-            renderValue={selected => selected.join(', ')}
-          // MenuProps={MenuProps}
-          >
-            {this.props.cats ? this.props.cats.map(catItem => (
-              <MenuItem key={catItem._id} value={catItem.name}>
-                <Checkbox checked={this.state.categories.indexOf(catItem.name) > -1} />
-                <ListItemText primary={catItem.name} />
-              </MenuItem>
-            )) : null}
-          </Select>
-        </FormControl> 
-      </FormGroup>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      {/* // <FormGroup>
-      //   <label className="form__controller--label"> متن دوره </label>
-      //    <Editor data={this.state.courseText}
-      //     chagedData={this.onEditorChange}
-      //   /> 
-
-      //   <textarea name="courseText" onChange={this.handleChange} />
-      // </FormGroup> 
-
-
-
-
-
-
-
-{/* 
-      // <FormGroup>
-      //   <ImageUploader />
-      // </FormGroup> 
-
-
-
-
-
-
-*/
